629a3491f2cb1c622aef100ba1c7b9b027892d1d
Brian Stone <brian@axds.co>
2025-02-21 18:35:49 -0800

    gah - fix objects duh

--- src/Form/Components/FieldCreator.tsx
+++ src/Form/Components/FieldCreator.tsx
@@ -91,7 +91,7 @@ const OneOfMultiple = ({
           value={value}
           onChange={(v) => {
             const newValues = [...values]
-            newValues[index] = String(v)
+            newValues[index] = v as IValueType
             onChange(newValues)
           }}
         />
