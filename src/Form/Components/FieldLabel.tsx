7ed2659f2eeabbdce0c5504914c2af795358c37c
Brian Stone <brian@axds.co>
2025-03-14 12:30:22 -0700

    display field descriptions with labels

--- src/Form/Components/FieldLabel.tsx
+++ src/Form/Components/FieldLabel.tsx
@@ -3,12 +3,26 @@ import React, { type ReactElement } from ***REMOVED***react***REMOVED***
 
 export const FieldLabelText = (field: IFormField): ReactElement => {
   return (
-    <strong>{field.label} { field.required === true ? <span className=***REMOVED***text-red-500***REMOVED***>*</span> : ***REMOVED******REMOVED***}</strong>
+    <p><strong>{field.label} { field.required === true ? <span className=***REMOVED***text-red-500***REMOVED***>*</span> : ***REMOVED******REMOVED***}</strong></p>
+  )
+}
+
+export const FieldDescriptionText = (field: IFormField): ReactElement => {
+  return (
+    <>{
+      field.description !== undefined
+        ? <p className=***REMOVED***text-xs py-2***REMOVED***>{field.description}</p>
+        : ***REMOVED******REMOVED***
+    }</>
   )
 }
 
 const FieldLabel = (field: IFormField): ReactElement => {
-  return <p className=***REMOVED***pb-2***REMOVED***><FieldLabelText {...field} /></p>
+  return <>
+    <FieldLabelText {...field} />
+    <FieldDescriptionText {...field} />
+
+  </>
 }
 
 export default FieldLabel
