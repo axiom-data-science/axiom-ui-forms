92cc43eee9ca2d0846e5325f445ccef9c4cbd4fa
Brian Stone <brian@axds.co>
2025-03-17 22:01:00 -0700

    add example of loading metadata and accessing from custom component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import FormManager from ***REMOVED***@/Form/Manage/Manage***REMOVED***
-import React, { useState, type ReactElement } from ***REMOVED***react***REMOVED***
+import React, { createContext, useContext, useState, type ReactElement } from ***REMOVED***react***REMOVED***
 import { BrowserRouter, Route, Routes } from ***REMOVED***react-router-dom***REMOVED***
 import SetTester from ***REMOVED***@/SetTester***REMOVED***
 import MapTester from ***REMOVED***@/Form/MapTester***REMOVED***
@@ -11,6 +11,7 @@ import { type INumberField, type IForm, type IFormValues, type IFieldInputProps 
 import Form from ***REMOVED***@/Form/Creator/FormCreator***REMOVED***
 import FieldLabel from ***REMOVED***@/Form/Components/FieldLabel***REMOVED***
 import { Slider } from ***REMOVED***@axdspub/axiom-ui-utilities***REMOVED***
+import ExternalMetadataExample from ***REMOVED***@/Form/ExternalMetadataExample***REMOVED***
 
 const PagedFormWrap = (): ReactElement => {
   const formValueState = useState<IFormValues>({})
@@ -32,10 +33,14 @@ const WizardFormWrap = (): ReactElement => {
   )
 }
 
+interface ICustomFormProp { label: string }
+const CustomContext = createContext<ICustomFormProp>({ label: ***REMOVED******REMOVED*** })
+
 const CustomElementFormWrap = (): ReactElement => {
   const formValueState = useState<IFormValues>({})
   return (
-    <div>
+    <CustomContext.Provider value={{ label: ***REMOVED***Custom Label***REMOVED*** }}>
+      <div>
       <Form
         form={customElementFormJson as IForm}
         formValueState={formValueState}
@@ -43,12 +48,14 @@ const CustomElementFormWrap = (): ReactElement => {
         urlNavigable={false}
         inputOverrides={{
           ***REMOVED***custom:number***REMOVED***: ({ field, value, onChange }: IFieldInputProps) => {
+            const { label: labelFromContext } = useContext(CustomContext)
             const numberField = field as INumberField
             const [tempValue, setTempValue] = useState<number>(value !== undefined && value !== null ? +value : 0)
             const min = numberField?.constraints?.min ?? 0
             const max = numberField?.constraints?.max ?? 100
             const step = Number(numberField?.settings?.step ?? (max - min) / 100)
             return (<div>
+              <h2 className=***REMOVED***p-4 text-xl bg-rose-800 text-white***REMOVED***>{labelFromContext}</h2>
               <FieldLabel {...field} />
               <div className=***REMOVED***flex flex-row gap-4***REMOVED***>
                 <p className=***REMOVED***font-bold w-[80px]***REMOVED***>{tempValue}</p>
@@ -60,22 +67,21 @@ const CustomElementFormWrap = (): ReactElement => {
                   max={max}
                   onChange={(v: number): void => {
                     setTempValue(v)
-                  }}
+                  } }
                   onChangeComplete={(v: number): void => {
                     setTempValue(v)
                     onChange(v)
-                  }}
+                  } }
                   id={field.id}
                   testId={field.id}
-                  step={step}
-                  />
+                  step={step} />
               </div>
             </div>)
           }
-        }}
-        />
+        }} />
       <pre className=***REMOVED***p-20 bg-slate-200 text-xs***REMOVED***>{JSON.stringify(formValueState[0], null, 2)}</pre>
     </div>
+    </CustomContext.Provider>
   )
 }
 
@@ -100,6 +106,7 @@ const App = (): ReactElement => {
             <Route path="/custom-form-element" element={<CustomElementFormWrap />}>
               <Route path=***REMOVED*******REMOVED*** element={<CustomElementFormWrap />} />
             </Route>
+            <Route path=***REMOVED***/custom-element-context***REMOVED*** element={<ExternalMetadataExample />} />
           </Routes>
         </BrowserRouter>
     </div>
---
+++ src/Form/ExternalMetadataExample.tsx
@@ -0,0 +1,159 @@
+import { FormCreator } from ***REMOVED***@/Form***REMOVED***
+import FieldLabel from ***REMOVED***@/Form/Components/FieldLabel***REMOVED***
+import { type IFieldInputProps, type IForm, type IFormValues } from ***REMOVED***@/Form/Creator/FormCreatorTypes***REMOVED***
+import { Loader, SelectInput } from ***REMOVED***@axdspub/axiom-ui-utilities***REMOVED***
+import { QueryClient, QueryClientProvider, useQuery } from ***REMOVED***@tanstack/react-query***REMOVED***
+import React, { createContext, useContext, useState, type ReactElement } from ***REMOVED***react***REMOVED***
+
+const form: IForm = {
+  id: ***REMOVED***externalMetadata***REMOVED***,
+  label: ***REMOVED***External Metadata Example***REMOVED***,
+  fields: [
+    {
+      id: ***REMOVED***parameter***REMOVED***,
+      label: ***REMOVED***Select parameter and units when available***REMOVED***,
+      type: ***REMOVED***custom:parameter***REMOVED***,
+      required: true
+    }
+  ]
+}
+
+interface IOikosUnit { id: string, label: string, unit: string }
+interface IOikosContext {
+  ***REMOVED***sensorParameters***REMOVED***: Array<{ id: string }>
+  ***REMOVED***units***REMOVED***: IOikosUnit[]
+  ***REMOVED***parameters***REMOVED***: Array<{ id: string, label: string, parameterName: string, idParameterType: number }>
+  ***REMOVED***parameterTypes***REMOVED***: Array<{ id: number, label: string }>
+  ***REMOVED***parameterTypeUnits***REMOVED***: Array<{ parameterTypeDefault: boolean, idParameterType: number, idUnit: string }>
+}
+
+const OikosContext = createContext<IOikosContext>({
+  sensorParameters: [],
+  units: [],
+  parameters: [],
+  parameterTypes: [],
+  parameterTypeUnits: []
+})
+
+interface IParameterValue { urn?: string, unit?: string }
+
+const getUnitsForParameter = (parameterId: string, context: IOikosContext): IOikosUnit[] => {
+  const { parameterTypeUnits, units } = context
+  const parameterTypeUnitIds = parameterTypeUnits
+    .filter((ptu) => ptu.idParameterType === context.parameters.find((p) => p.id === parameterId)?.idParameterType)
+    .map((ptu) => ptu.idUnit)
+  return units.filter((u) => parameterTypeUnitIds.includes(u.id))
+}
+
+const ExternalMetadataExample = (): ReactElement => {
+  const { data, isLoading, error } = useQuery<IOikosContext>({
+    queryKey: [***REMOVED***externalMetadata***REMOVED***],
+    queryFn: async () => {
+      const response = await fetch(***REMOVED***https://oikos.axds.co/rest/context***REMOVED***)
+      if (!response.ok) {
+        throw new Error(***REMOVED***Network response was not ok***REMOVED***)
+      }
+      return await response.json()
+    }
+  })
+
+  const formValueState = useState<IFormValues>({})
+
+  return (
+        <>
+            {
+                isLoading || data === undefined
+                  ? (
+                    <Loader className="pt-20" />
+                    )
+                  : error
+                    ? (
+                        <div className=***REMOVED***p-4 text-rose-800***REMOVED***>Error: {error.message}</div>
+                      )
+                    : (
+                        <OikosContext.Provider value={data}>
+                          <FormCreator
+                            form={form}
+                            formValueState={formValueState}
+                            inputOverrides={{
+                              ***REMOVED***custom:parameter***REMOVED***: ({ field, value, onChange }: IFieldInputProps): ReactElement => {
+                                const paramValue = value as IParameterValue
+                                const oikos = useContext(OikosContext)
+                                const { parameters } = oikos
+                                const parametersById = Object.fromEntries(parameters.map((p) => [p.id, p]))
+                                const parametersByUrn = Object.fromEntries(parameters.map((p) => [p.parameterName, p]))
+                                const units = paramValue?.urn !== undefined && parametersByUrn[paramValue.urn] !== undefined
+                                  ? getUnitsForParameter(parametersByUrn[paramValue.urn].id, oikos)
+                                  : undefined
+
+                                return (<div>
+                                  <FieldLabel {...field} />
+                                  <div className=***REMOVED***flex flex-row gap-4 text-sm***REMOVED***>
+                                    <SelectInput
+                                        id=***REMOVED***parameter***REMOVED***
+                                        testId=***REMOVED***parameter***REMOVED***
+                                        options={parameters.map(
+                                          (p) => ({
+                                            label: p.label,
+                                            value: p.id
+                                          })
+                                        ).sort((a, b) => a.label.localeCompare(b.label))
+                                        }
+                                        value={paramValue?.urn !== undefined
+                                          ? parametersByUrn[paramValue.urn]?.id
+                                          : undefined}
+                                        onChange={(v) => {
+                                          onChange({
+                                            urn: v?.value !== undefined
+                                              ? parametersById[v.value]?.parameterName
+                                              : undefined,
+                                            unit: getUnitsForParameter(v?.value ?? ***REMOVED******REMOVED***, oikos)[0]?.unit
+                                          })
+                                        }}
+                                        placeholder=***REMOVED***Select parameter***REMOVED***
+                                      />
+                                      {
+                                        units !== undefined && units.length > 0
+                                          ? <SelectInput
+                                        id=***REMOVED***unit***REMOVED***
+                                        testId=***REMOVED***unit***REMOVED***
+                                        options={units.map((u) => ({ label: u.label, value: u.unit })).sort((a, b) => a.label.localeCompare(b.label))}
+                                        value={paramValue?.unit}
+                                        onChange={(v) => {
+                                          onChange({ urn: paramValue?.urn, unit: v?.value })
+                                        }}
+                                        includePrompt={false}
+                                        />
+                                          : ***REMOVED***--***REMOVED***
+                                      }
+                                    </div>
+                                  </div>
+
+                                )
+                              }
+                            }}
+                            />
+                            <pre className=***REMOVED***bg-slate-100 text-xs p-10***REMOVED***>
+                              {JSON.stringify(formValueState[0], null, 2)}
+                            </pre>
+                        </OikosContext.Provider>
+                      )
+            }
+
+        </>
+
+  )
+}
+
+const ExternalMetadataExampleLoader = (): ReactElement => {
+  const queryClient = new QueryClient()
+  return (
+    <div className=***REMOVED***p-20***REMOVED***>
+    <QueryClientProvider client={queryClient}>
+      <ExternalMetadataExample />
+    </QueryClientProvider>
+    </div>
+  )
+}
+
+export default ExternalMetadataExampleLoader
