57226fb4978f6c994f81e6be04fb0f5e4b2dd9f3
Brian Stone <brian@axds.co>
2025-03-20 15:21:09 -0700

    allow description display with checkbox

--- src/Form/Components/Inputs/Boolean.tsx
+++ src/Form/Components/Inputs/Boolean.tsx
@@ -1,11 +1,16 @@
 import { FieldLabelText } from ***REMOVED***@/Form/Components/FieldLabel***REMOVED***
 import { type IFieldInputProps } from ***REMOVED***@/Form/Creator/FormCreatorTypes***REMOVED***
-import { Checkbox } from ***REMOVED***@axdspub/axiom-ui-utilities***REMOVED***
+import { Checkbox, Tooltip } from ***REMOVED***@axdspub/axiom-ui-utilities***REMOVED***
+import { InfoCircledIcon } from ***REMOVED***@radix-ui/react-icons***REMOVED***
 import React, { type ReactElement } from ***REMOVED***react***REMOVED***
 
 const BooleanInput = ({ field, onChange, value }: IFieldInputProps): ReactElement => {
   const initialValue = value !== undefined ? value : false
-  return <Checkbox id={field.id} testId={field.id} label={<FieldLabelText {...field} />} value={Boolean(initialValue)} onChange={(e) => {
+  return <Checkbox id={field.id} testId={field.id} label={<><FieldLabelText {...field} /> {
+    field.description !== undefined
+      ? <Tooltip content={field.description}><InfoCircledIcon /></Tooltip>
+      : ***REMOVED******REMOVED***
+  }</>} value={Boolean(initialValue)} onChange={(e) => {
     onChange(e)
   }} />
 }
