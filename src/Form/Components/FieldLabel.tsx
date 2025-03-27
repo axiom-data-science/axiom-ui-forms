129356288a231bb9895f213019a9a618ed801790
Brian Stone <brian@axds.co>
2025-03-27 11:17:22 -0700

    allow markdown in field label and field description and add tooltip description option for use with checkboxes

--- src/Form/Components/FieldLabel.tsx
+++ src/Form/Components/FieldLabel.tsx
@@ -1,9 +1,26 @@
 import { type IFormField } from ***REMOVED***@/Form/Creator/FormCreatorTypes***REMOVED***
+import { Tooltip } from ***REMOVED***@axdspub/axiom-ui-utilities***REMOVED***
+import { InfoCircledIcon } from ***REMOVED***@radix-ui/react-icons***REMOVED***
 import React, { type ReactElement } from ***REMOVED***react***REMOVED***
+import Markdown from ***REMOVED***react-markdown***REMOVED***
+
+export const MarkdownText = ({ children }: { children: string | null | undefined }): ReactElement => {
+  return <Markdown components={{
+    p: ({ children }) => <>{children}</>
+  }}>{children}</Markdown>
+}
+
+export const FieldDescriptionTooltip = (field: IFormField): ReactElement => {
+  return (
+    field.description !== undefined
+      ? <Tooltip content={field.description}><InfoCircledIcon /></Tooltip>
+      : <></>
+  )
+}
 
 export const FieldLabelText = (field: IFormField): ReactElement => {
   return (
-    <strong>{field.label} { field.required === true ? <span className=***REMOVED***text-red-500***REMOVED***>*</span> : ***REMOVED******REMOVED***}</strong>
+    <strong><MarkdownText>{field.label}</MarkdownText> { field.required === true ? <span className=***REMOVED***text-red-500***REMOVED***>*</span> : ***REMOVED******REMOVED***}</strong>
   )
 }
 
@@ -11,7 +28,7 @@ export const FieldDescriptionText = (field: IFormField): ReactElement => {
   return (
     <>{
       field.description !== undefined
-        ? <p className=***REMOVED***text-xs py-2***REMOVED***>{field.description}</p>
+        ? <p className=***REMOVED***text-xs py-2***REMOVED***><MarkdownText>{field.description}</MarkdownText></p>
         : ***REMOVED******REMOVED***
     }</>
   )
