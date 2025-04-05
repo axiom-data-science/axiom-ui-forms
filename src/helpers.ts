857cb8aac4aab18b9306b4a9276737c32d21cc64
Brian Stone <brian@axds.co>
2025-04-05 09:39:43 -0700

    remove unused

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,3 @@
-import { type IValueType, type IFormValues } from ***REMOVED***@/Form/Creator/FormCreatorTypes***REMOVED***
 import { decode, encode } from ***REMOVED***cbor-x***REMOVED***
 
 export function jsonToBase64<T = JSON> (json: T): string {
@@ -27,10 +26,4 @@ export function updateUrlParam (param: string, value?: string | boolean | number
 export function getQueryParam (param: string): string | null {
   const u = new URL(window.location.href)
   return u.searchParams.get(param)
-}
-
-export function typeToFormValues (m: any = {}): IFormValues {
-  return Object.fromEntries(Object.keys(m).map(k => {
-    return [String(k), m[k] as IValueType]
-  }))
 }
