cd2d2569f8c31525c45a04d6e5902f8d3d9649de
Brian Stone <brian@axds.co>
2025-06-25 10:50:28 -0800

    style markdown links

--- src/Form/Components/InlineMarkdown.tsx
+++ src/Form/Components/InlineMarkdown.tsx
@@ -3,7 +3,8 @@ import Markdown from ***REMOVED***react-markdown***REMOVED***
 
 const InlineMarkdown = ({ children }: { children: string | null | undefined }): ReactElement => {
   return <Markdown components={{
-    p: ({ children }) => <>{children}</>
+    p: ({ children }) => <>{children}</>,
+    a: ({ children, href }) => <a href={href} target={href?.match(/^http/) ? ***REMOVED***_blank***REMOVED*** : undefined} rel="noopener noreferrer" className="text-blue-500 hover:underline">{children}</a>
   }}>{children}</Markdown>
 }
 
