329368f12ff3bb684ddb92a4c9869dcae63d56f9
Brian Stone <brian@axds.co>
2025-06-25 11:12:36 -0800

    add webcoos metadata objects

---
+++ src/Form/WebCOOS/cam-api-assets-example-curl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl --silent ***REMOVED***https://cam-api.srv.axds.co/webcoos/api/v1/assets/***REMOVED*** \
+  -H ***REMOVED***Accept: application/json***REMOVED*** \
+  -H ***REMOVED***Authorization: Token ...***REMOVED***
+
