ea44525ff7db1c2e231bce4b1c2c41e66c83bf77
Brian Stone <brian@axds.co>
2025-07-18 17:58:06 -0800

    make objects in path objects

--- src/utils/getters.test.ts
+++ src/utils/getters.test.ts
@@ -28,7 +28,7 @@ describe(***REMOVED***getters.ts***REMOVED***, () => {
       const field: IFormField = {
         id: ***REMOVED***field3***REMOVED***,
         type: ***REMOVED***text***REMOVED***,
-        path: [{ id: ***REMOVED***parent***REMOVED***, multiple: true, type: ***REMOVED***text***REMOVED*** }, { id: ***REMOVED***child***REMOVED***, type: ***REMOVED***text***REMOVED*** }, { id: ***REMOVED***field3***REMOVED***, type: ***REMOVED***text***REMOVED*** }]
+        path: [{ id: ***REMOVED***parent***REMOVED***, multiple: true, type: ***REMOVED***object***REMOVED***, fields: [] }, { id: ***REMOVED***child***REMOVED***, type: ***REMOVED***object***REMOVED***, fields: [] }, { id: ***REMOVED***field3***REMOVED***, type: ***REMOVED***text***REMOVED*** }]
       }
       const result = makeJsonPath(field)
       expect(result).toBe(***REMOVED***parent[0].child.field3***REMOVED***)
@@ -48,7 +48,7 @@ describe(***REMOVED***getters.ts***REMOVED***, () => {
         type: ***REMOVED***text***REMOVED***,
         multiple: true,
         index: 2,
-        path: [{ id: ***REMOVED***parent***REMOVED***, multiple: true, type: ***REMOVED***text***REMOVED*** }, { id: ***REMOVED***child***REMOVED***, type: ***REMOVED***text***REMOVED*** }, { id: ***REMOVED***fieldWithIndex***REMOVED***, type: ***REMOVED***text***REMOVED***, multiple: true, index: 2 }]
+        path: [{ id: ***REMOVED***parent***REMOVED***, multiple: true, type: ***REMOVED***object***REMOVED***, fields: [] }, { id: ***REMOVED***child***REMOVED***, type: ***REMOVED***object***REMOVED***, fields: [] }, { id: ***REMOVED***fieldWithIndex***REMOVED***, type: ***REMOVED***text***REMOVED***, multiple: true, index: 2 }]
       }
       const result = makeJsonPath(field)
       expect(result).toBe(***REMOVED***parent[0].child.fieldWithIndex[2]***REMOVED***)
