c97a101e2aeee98884ef7994b4f48cbde179b2d8
Brian Stone <brian@axds.co>
2025-09-03 11:20:20 -0800

    format external markdown links with new window icon

--- src/Form/Components/InlineMarkdown.tsx
+++ src/Form/Components/InlineMarkdown.tsx
@@ -1,10 +1,16 @@
+import { OpenInNewWindowIcon } from ***REMOVED***@radix-ui/react-icons***REMOVED***
 import React, { type ReactElement } from ***REMOVED***react***REMOVED***
 import Markdown from ***REMOVED***react-markdown***REMOVED***
 
 const InlineMarkdown = ({ children }: { children: string | null | undefined }): ReactElement => {
   return <Markdown components={{
     p: ({ children }) => <>{children}</>,
-    a: ({ children, href }) => <a href={href} target={href?.match(/^http/) ? ***REMOVED***_blank***REMOVED*** : undefined} rel="noopener noreferrer" className="text-blue-500 hover:underline">{children}</a>,
+    a: ({ children, href }) => {
+      const isExternal = href?.match(/^http/)
+      return (
+      <a href={href} target={isExternal ? ***REMOVED***_blank***REMOVED*** : undefined} rel="noopener noreferrer" className="text-blue-500 hover:underline">{children}{isExternal ? <OpenInNewWindowIcon className=***REMOVED***inline ml-1***REMOVED*** /> : ***REMOVED******REMOVED***}</a>
+      )
+    },
     ul: ({ children }) => <ul className="list-disc list-inside ml-4 my-2">{children}</ul>,
   }}>{children}</Markdown>
 }
