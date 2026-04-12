56061787f2a4544cd6eb035f5d542df9d50294ef
Brian Stone <brian@axds.co>
2026-04-12 10:11:57 -0800

    add space around tooltip button for boolean field

--- src/Form/Components/Inputs/Boolean.tsx
+++ src/Form/Components/Inputs/Boolean.tsx
@@ -1,31 +1,38 @@
-import { FieldDescriptionText, FieldDescriptionTooltip, FieldLabelText } from ***REMOVED***@/Form/Components/FieldLabel***REMOVED***
+import {
+  FieldDescriptionText,
+  FieldDescriptionTooltip,
+  FieldLabelText,
+} from ***REMOVED***@/Form/Components/FieldLabel***REMOVED***
 import { type IFieldInputProps } from ***REMOVED***@/Form/Creator/FormCreatorTypes***REMOVED***
 import { Checkbox } from ***REMOVED***@axdspub/axiom-ui-utilities***REMOVED***
 import React, { type ReactElement } from ***REMOVED***react***REMOVED***
 
 const BooleanInput = ({ field, onChange, value, disabled }: IFieldInputProps): ReactElement => {
   const initialValue = value !== undefined ? value : false
-  return <Checkbox 
-    id={field.id} 
-    testId={field.id} 
-    disabled={disabled} 
-    value={Boolean(initialValue)} 
-    onChange={(e) => {
-      onChange(e)
-    }}
-    label={<>
-      <FieldLabelText 
-        field={field} 
-        disabled={disabled} 
-        value={value}
-        onChange={onChange} 
-      /> {
-        (field.settings?.descriptionPresentation === ***REMOVED***inline***REMOVED*** || field.settings?.descriptionPresentation === undefined)
-          ? <FieldDescriptionTooltip field={field} disabled={disabled} />
-          : <FieldDescriptionText field={field} disabled={disabled} />
-      }</>
-    }
-  />
+  return (
+    <Checkbox
+      id={field.id}
+      testId={field.id}
+      disabled={disabled}
+      value={Boolean(initialValue)}
+      onChange={(e) => {
+        onChange(e)
+      }}
+      label={
+        <>
+          <FieldLabelText field={field} disabled={disabled} value={value} onChange={onChange} />{***REMOVED*** ***REMOVED***}
+          {field.settings?.descriptionPresentation === ***REMOVED***inline***REMOVED*** ||
+          field.settings?.descriptionPresentation === undefined ? (
+            <span className="ml-1">
+              <FieldDescriptionTooltip field={field} disabled={disabled} />
+            </span>
+          ) : (
+            <FieldDescriptionText field={field} disabled={disabled} />
+          )}
+        </>
+      }
+    />
+  )
 }
 
 export default BooleanInput
