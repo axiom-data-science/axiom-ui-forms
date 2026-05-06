a43109e8def5d3251e13c661aa6130931c7ed360
Brian Stone <brian@axds.co>
2026-05-05 16:54:36 -0800

    start new objectlist type - in prog

---
+++ src/Form/Components/Inputs/ObjectList.tsx
@@ -0,0 +1,36 @@
+import ObjectInput from "@/Form/Components/Inputs/Object"
+import { IFieldInputProps, IObjectListField, IValueType } from "@/Form/Creator/FormCreatorTypes"
+import { ReactElement } from "react"
+
+const ObjectListInput = ({ field, onChange, value, disabled }: IFieldInputProps): ReactElement => {
+
+
+    const f: IObjectListField = field as IObjectListField
+    const keyField = f.fields.find(_f=>_f.id === f.settings.keyField)
+    let configError: string | null = null
+    if(keyField === undefined || keyField === null){
+        configError = `Key field ${f.settings.keyField} does not point at a valid field`
+    }
+    if(keyField?.multiple){
+        configError = ***REMOVED***Key field cannot be a multiple***REMOVED***
+    }
+    if(keyField?.type === ***REMOVED***object***REMOVED*** || keyField?.type === ***REMOVED***number***REMOVED*** || keyField?.type === ***REMOVED***boolean***REMOVED*** || keyField?.type === ***REMOVED***checkbox***REMOVED***){
+        configError = ***REMOVED***Key field cannot be an object, numeric or boolean type***REMOVED***
+    }
+    const oC = (value: IValueType | IValueType[]): void => {
+        console.log(***REMOVED***hi..***REMOVED***)
+        onChange(value)
+    }
+
+
+    return <>
+        <ObjectInput 
+            field={field}
+            onChange={oC}
+            value={value}
+            disabled={disabled}
+        />
+    </>
+
+
+}
