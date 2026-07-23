c847c8fc3b9d11c7f2129cb7c29f7ca3badce0e0
Brian Stone <brian@axds.co>
2026-07-22 19:35:23 -0800

    expose state selector in package bundle

--- src/Form/Components/Inputs/index.tsx
+++ src/Form/Components/Inputs/index.tsx
@@ -4,6 +4,7 @@ export { default as NumberInput } from ***REMOVED***./Number***REMOVED***
 export { default as TextInput } from ***REMOVED***./String***REMOVED***
 export { default as LongTextInput } from ***REMOVED***./LongString***REMOVED***
 export { default as SelectInput } from ***REMOVED***./SingleSelect***REMOVED***
+export { default as StateSelectorInput } from ***REMOVED***./StateSelectorInput***REMOVED***
 export { default as GeoJSONInput } from ***REMOVED***./GeoJSONInputLoader***REMOVED***
 export { default as GeometryInput } from ***REMOVED***./Geometry***REMOVED***
 export { default as DateTimeInput } from ***REMOVED***./DateTime***REMOVED***
