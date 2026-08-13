0e671a44c60b048a4e3fd311797276424c90c347
Trevor Golden <trevor@axiomdatascience.com>
2026-08-13 10:20:36 -0500

    Type useState as undefined-compatible to match FormWithEditorOverlay props
    ROS-9

--- src/Form/PAM/PAMForm.tsx
+++ src/Form/PAM/PAMForm.tsx
@@ -1,12 +1,14 @@
 import FormWithEditorOverlay from ***REMOVED***@/Form/FormWithEditorOverlay***REMOVED***
 import { ReactElement, useState } from ***REMOVED***react***REMOVED***
+import { type JSONSchema6 } from ***REMOVED***json-schema***REMOVED***
 import { IFormFieldOverride } from ***REMOVED***@/library***REMOVED***
+import { type IFormOverride } from ***REMOVED***@/Form/Creator/FormCreatorTypes***REMOVED***
 import { pamSchema, pamForm } from ***REMOVED***./config***REMOVED***
 
 const PAMForm = (): ReactElement => {
-  const schemaState = useState(pamSchema)
+  const schemaState = useState<JSONSchema6 | undefined>(pamSchema)
   const fieldOverrideState = useState<IFormFieldOverride[]>([])
-  const formOverrideState = useState(pamForm)
+  const formOverrideState = useState<IFormOverride | undefined>(pamForm)
 
   return (
     <FormWithEditorOverlay
