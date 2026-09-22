93fc6594d98adcc5f1aad599e4a0313b618f19f5
Brian Stone <brian@axds.co>
2026-09-22 12:59:06 -0800

    get rid of webcoos sample code

--- src/WebCOOS/webcoos-assets-example-curl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl --silent ***REMOVED***https://app.webcoos.org/webcoos/api/v1/assets/?source=webcoos***REMOVED*** \
-  -H ***REMOVED***Accept: application/json***REMOVED*** \
-  -H ***REMOVED***Authorization: Token 4af70560667182ab2a37fbca973cfdde4ea058ce***REMOVED***
-
